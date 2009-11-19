54b8a1ea1ce340c346f3bf90d214f532ee49a783
Matthew Schinckel <matt@schinckel.net>
2009-11-20 10:13:21 +1030

    Use `open` instead of appscript or osascript. Use `os.path.realpath` to ensure we are using the canonical pathname, to prevent new windows being opened, even if the file is already open.

--- PdbTextMateSupport.py
+++ PdbTextMateSupport.py
@@ -1,23 +1,13 @@
-from os.path import exists
+from os.path import exists, realpath
 from os import system
-
-have_appscript = False
-try:
-    from appscript import app
-    have_appscript = True
-except:
-    pass
 
 def mate(self):
     frame, lineno = self.stack[self.curindex]
     filename = self.canonic(frame.f_code.co_filename)
     if exists(filename):
-        tm_url = 'txmt://open?url=file://%s&line=%d&column=2' % (filename, lineno)
-        if have_appscript:
-            app("TextMate").get_url(tm_url)
-        else:
-            osa_cmd = 'tell application "TextMate" to get url "%s"' % tm_url
-            system('osascript -e \'%s\'' % osa_cmd)
+        filename = realpath(filename)
+        tm_url = 'txmt://open?url=file://%s\&line=%d\&column=2' % (filename, lineno)
+        system('open %s' % tm_url)
 
 def preloop(self):
     mate(self)
