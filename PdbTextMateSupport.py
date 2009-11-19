65e15f8a0e46c5b59219583b5405927528f058cf
Matthew Schinckel <matt@schinckel.net>
2009-11-20 10:15:46 +1030

    Re-enabled appscript support.

--- PdbTextMateSupport.py
+++ PdbTextMateSupport.py
@@ -1,13 +1,23 @@
 from os.path import exists, realpath
 from os import system
 
+have_appscript = False
+try:
+    from appscript import app
+    have_appscript = True
+except:
+    pass
+
 def mate(self):
     frame, lineno = self.stack[self.curindex]
     filename = self.canonic(frame.f_code.co_filename)
     if exists(filename):
         filename = realpath(filename)
-        tm_url = 'txmt://open?url=file://%s\&line=%d\&column=2' % (filename, lineno)
-        system('open %s' % tm_url)
+        tm_url = 'txmt://open?url=file://%s&line=%d&column=2' % (filename, lineno)
+        if have_appscript:
+            app("TextMate").get_url(tm_url)
+        else:
+            system('open %s' % tm_url)
 
 def preloop(self):
     mate(self)
